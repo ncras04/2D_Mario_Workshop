7e5daffcc94e8473ae2019cd98c8a71bccd06635
ncras04 <nicolasaschmitt@googlemail.com>
2022-04-24 22:47:54 +0200

    Depression Camp

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -32,18 +32,37 @@ public class PlayerController : MonoBehaviour
     {
         BoxCollider2D tmp = GetComponent<BoxCollider2D>();
         m_groundCheckPosY = tmp.size.y;
-        m_groundCheckSize = new Vector2(tmp.size.x, 0.3f);
-        m_groundCheckPos = new Vector2(transform.position.x, transform.position.y - m_groundCheckPosY);
+        m_groundCheckSize = new Vector2(tmp.size.x, 0.5f);
+        m_groundCheckPos = new Vector2(transform.position.x, transform.position.y - m_groundCheckPosY * 0.5f) * transform.localScale;
 
         m_rigidbody = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
+        m_groundCheckPos = new Vector2(transform.position.x, transform.position.y - m_groundCheckPosY * 0.5f) * transform.localScale;
+        m_isGrounded = Physics2D.OverlapBox(m_groundCheckPos, m_groundCheckSize * transform.localScale * 0.5f, 0f, m_groundLayer);
+
+        if (m_isJumping)
+        {
+            if (m_jumpCounter > 0)
+            {
+                m_rigidbody.velocity = Vector2.up * m_jumpForce;
+                //m_rigidbody.AddForce(Vector2.up * m_jumpForce * 10f, ForceMode2D.Impulse);
+                m_jumpCounter -= Time.deltaTime;
+            }
+            else
+                m_isJumping = false;
+        }
+
+        m_rigidbody.velocity = new Vector2(m_moveDirection * m_movementSpeed, m_rigidbody.velocity.y);
+        //m_rigidbody.AddForce(m_moveDirection * m_movementSpeed * 1000f * Time.fixedDeltaTime, ForceMode2D.Force);
+
+        ClearConsole();
+        Debug.Log(m_isGrounded);
+
         if (Input.GetKeyUp(KeyCode.Space))
             m_isJumping = false;
-
-        m_groundCheckPos = new Vector2(transform.position.x, transform.position.y - m_groundCheckPosY);
 
         m_moveDirection = Input.GetAxisRaw("Horizontal");
 
@@ -57,22 +76,23 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        m_isGrounded = Physics2D.OverlapBox(m_groundCheckPos, m_groundCheckSize, 0f, m_groundLayer);
 
-        if (m_isJumping)
-        {
-            if (m_jumpCounter > 0)
-            {
-                m_rigidbody.velocity = Vector2.up * m_jumpForce;
-                //m_rigidbody.AddForce(Vector2.up * m_jumpForce * 10f, ForceMode2D.Impulse);
-                m_jumpCounter -= Time.fixedDeltaTime;
-            }
-            else
-                m_isJumping = false;
-        }
-
-        m_rigidbody.velocity = new Vector2(m_moveDirection * m_movementSpeed, m_rigidbody.velocity.y);
-        //m_rigidbody.AddForce(m_moveDirection * m_movementSpeed * 1000f * Time.fixedDeltaTime, ForceMode2D.Force);
 
     }
+
+    static void ClearConsole()
+    {
+        var logEntries = System.Type.GetType("UnityEditor.LogEntries, UnityEditor.dll");
+
+        var clearMethod = logEntries.GetMethod("Clear", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public);
+
+        clearMethod.Invoke(null, null);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(m_groundCheckPos, 0.1f);
+        Gizmos.DrawCube(m_groundCheckPos, Physics2D.OverlapBox(m_groundCheckPos, m_groundCheckSize * transform.localScale, 0f, m_groundLayer).bounds.size);
+    }
+
 }
