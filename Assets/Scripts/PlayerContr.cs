73263c0408e6580e5ce0e539c303f624ea5d13f0
ncras04 <nicolasaschmitt@googlemail.com>
2022-04-28 07:37:06 +0200

    StateMachine done

--- Assets/Scripts/PlayerContr.cs
+++ Assets/Scripts/PlayerContr.cs
@@ -48,6 +48,8 @@ public class PlayerContr : MonoBehaviour
 
     private EPlayerStates currentState;
 
+    private float xVelocity;
+
     void Start()
     {
         BoxCollider2D tmp = GetComponent<BoxCollider2D>();
@@ -62,13 +64,11 @@ public class PlayerContr : MonoBehaviour
         horizontalDirection = Input.GetAxisRaw("Horizontal");
 
         groundCheckPos = new Vector2(transform.position.x, transform.position.y - groundCheckPosY * 0.5f) * transform.localScale;
-        hit = Physics2D.BoxCast(groundCheckPos, groundCheckSize, 0f, Vector2.down, 0.5f, groundAndEnemyLayer);
+        hit = BoxCast(groundCheckPos, groundCheckSize, 0f, Vector2.down, 0.1f, groundAndEnemyLayer);
 
-        isGrounded = hit.collider != null;
+        isGrounded = hit.collider;
 
         currentState = CheckState(currentState);
-        Debug.Log(currentState);
-
     }
 
     private EPlayerStates CheckState(EPlayerStates _currentState)
@@ -175,15 +175,13 @@ public class PlayerContr : MonoBehaviour
 
     private bool CheckEnemyKill()
     {
-        if (hit.collider != null)
+        if (hit.collider.CompareTag("Enemy"))
         {
-            if (hit.collider.CompareTag("Enemy"))
-            {
-                rb.velocity = Vector2.up * ReboundForce;
-                Destroy(hit.collider.gameObject);
-                return true;
-            }
+            rb.velocity = Vector2.up * ReboundForce;
+            Destroy(hit.collider.gameObject);
+            return true;
         }
+
         return false;
     }
 
@@ -205,6 +203,63 @@ public class PlayerContr : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        Gizmos.DrawWireCube(groundCheckPos, groundCheckSize);
+
+        // Gizmos.DrawWireCube(groundCheckPos, groundCheckSize);
+    }
+
+    static public RaycastHit2D BoxCast(Vector2 origin, Vector2 size, float angle, Vector2 direction, float distance, int mask)
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(origin, size, angle, direction, distance, mask);
+
+        //Setting up the points to draw the cast
+        Vector2 p1, p2, p3, p4, p5, p6, p7, p8;
+        float w = size.x * 0.5f;
+        float h = size.y * 0.5f;
+        p1 = new Vector2(-w, h);
+        p2 = new Vector2(w, h);
+        p3 = new Vector2(w, -h);
+        p4 = new Vector2(-w, -h);
+
+        Quaternion q = Quaternion.AngleAxis(angle, new Vector3(0, 0, 1));
+        p1 = q * p1;
+        p2 = q * p2;
+        p3 = q * p3;
+        p4 = q * p4;
+
+        p1 += origin;
+        p2 += origin;
+        p3 += origin;
+        p4 += origin;
+
+        Vector2 realDistance = direction.normalized * distance;
+        p5 = p1 + realDistance;
+        p6 = p2 + realDistance;
+        p7 = p3 + realDistance;
+        p8 = p4 + realDistance;
+
+
+        //Drawing the cast
+        Color castColor = hit ? Color.red : Color.green;
+        Debug.DrawLine(p1, p2, castColor);
+        Debug.DrawLine(p2, p3, castColor);
+        Debug.DrawLine(p3, p4, castColor);
+        Debug.DrawLine(p4, p1, castColor);
+
+        Debug.DrawLine(p5, p6, castColor);
+        Debug.DrawLine(p6, p7, castColor);
+        Debug.DrawLine(p7, p8, castColor);
+        Debug.DrawLine(p8, p5, castColor);
+
+        Debug.DrawLine(p1, p5, Color.grey);
+        Debug.DrawLine(p2, p6, Color.grey);
+        Debug.DrawLine(p3, p7, Color.grey);
+        Debug.DrawLine(p4, p8, Color.grey);
+        if (hit)
+        {
+            Debug.DrawLine(hit.point, hit.point + hit.normal.normalized * 0.2f, Color.yellow);
+        }
+
+        return hit;
+
     }
 }
